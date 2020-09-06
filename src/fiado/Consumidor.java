1f96028e34725c64df4d9e05824af4555fcacca8
Lucas Massocco <lucas.massocco@gmail.com>
2020-09-06 15:40:49 -0300

    atualizacao

--- src/fiado/Consumidor.java
+++ src/fiado/Consumidor.java
@@ -2,7 +2,38 @@ package fiado;
 
 public class Consumidor {
 	
-	String nome;
-	int[] fiados;
+	private String nome;
+	private int[] fiados;
+	private int posicao = 0;
 
+	
+	// construtor - prepara o objeto para mim
+	public Consumidor(String nome) {
+		this.nome = nome;
+		this.fiados = new int[10]; //inicializa com um array de 10 posições
+		
+	}
+	
+	// Getter and Setter -  para resolver os Privates
+	
+	public String getNome( ) {
+		return this.nome;
+	}
+	
+	public void setNome (String nome ) {
+		this.nome = nome;
+	}
+	
+	public void registrarFiado(int valor) {
+		this.fiados[posicao] = valor;
+		posicao++;
+	}
+	
+	public int getFiado() {
+		int total = 0;
+		for (int fiado : fiados) {
+			total += fiado;
+		}
+		return total;
+	}
 }
